e8998fc5eaed518246bf48e9e7bc23899e2e1b31
Thiago Galvani <thiagopaiva99@gmail.com>
2022-01-26 15:41:23 -0300

    feat: generating initial version for available meals component

---
+++ src/screens/meals/components/available-meals/available-meals.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type AvailableMealsProps = {}
+
+const AvailableMeals: React.FC<AvailableMealsProps> = () => {
+    return (
+        <h1>available meals</h1>
+    )
+}
+
+export default AvailableMeals
