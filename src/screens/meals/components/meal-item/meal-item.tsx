40305a360a53c736b87208a736c55eb88cb0fdb2
Thiago Galvani <thiagopaiva99@gmail.com>
2022-01-26 16:25:40 -0300

    feat: generating initial version of meal item component

---
+++ src/screens/meals/components/meal-item/meal-item.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type MealItemProps = {
+
+}
+
+const MealItem: React.FC<MealItemProps> = () => {
+    return (
+        <h1>meal item</h1>
+    )
+}
+
+export { MealItem }
