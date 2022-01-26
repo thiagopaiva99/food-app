b34b7ba504108655e53d258c996dc7e378136bfa
Thiago Galvani <thiagopaiva99@gmail.com>
2022-01-26 16:25:09 -0300

    refactor: using card component on available meals component

--- src/screens/meals/components/available-meals/available-meals.tsx
+++ src/screens/meals/components/available-meals/available-meals.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Card } from '../../../../components'
 import { DUMMY_MEALS } from './available-meals.data'
 import { StyledMealsList, StyledMealsSection } from './available-meals.styles'
 
@@ -7,11 +8,13 @@ type AvailableMealsProps = {}
 const AvailableMeals: React.FC<AvailableMealsProps> = () => {
     return (
         <StyledMealsSection>
-            <StyledMealsList>
-                {DUMMY_MEALS.map(meal => (
-                    <li key={meal.id}>{meal.description}</li>
-                ))}
-            </StyledMealsList>
+            <Card>
+                <StyledMealsList>
+                    {DUMMY_MEALS.map(meal => (
+                        <li key={meal.id}>{meal.description}</li>
+                    ))}
+                </StyledMealsList>
+            </Card>
         </StyledMealsSection>
     )
 }
