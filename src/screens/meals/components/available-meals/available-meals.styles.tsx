445c0ea2100afb03e89c6c288d9fcaf98600f533
Thiago Galvani <thiagopaiva99@gmail.com>
2022-01-26 15:46:48 -0300

    feat: adding styles for available meals list

---
+++ src/screens/meals/components/available-meals/available-meals.styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledMealsSection = styled.section`
+    max-width: 60rem;
+    width: 90%;
+    margin: 2rem auto;
+`
+
+export const StyledMealsList = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+`
--- src/screens/meals/components/available-meals/available-meals.tsx
+++ src/screens/meals/components/available-meals/available-meals.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { DUMMY_MEALS } from './available-meals.data'
+import { StyledMealsList, StyledMealsSection } from './available-meals.styles'
 
 type AvailableMealsProps = {}
 
 const AvailableMeals: React.FC<AvailableMealsProps> = () => {
     return (
-        <h1>available meals</h1>
+        <StyledMealsSection>
+            <StyledMealsList>
+                {DUMMY_MEALS.map(meal => (
+                    <li key={meal.id}>{meal.description}</li>
+                ))}
+            </StyledMealsList>
+        </StyledMealsSection>
     )
 }
 
