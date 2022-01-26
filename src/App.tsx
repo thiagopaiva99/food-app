d12e9cadd8000c32c43c3359c36a1a3cb931b15b
Thiago Galvani <thiagopaiva99@gmail.com>
2022-01-26 16:08:36 -0300

    feat: adding meals list on the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Header } from './components';
+import { Meals } from './screens';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
