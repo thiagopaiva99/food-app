c8c23619b373e3d6fbbe5356049f28fe72f542dd
Thiago Galvani <thiagopaiva99@gmail.com>
2022-01-26 15:10:14 -0300

    feat: generating initial version for cart button

---
+++ src/components/cart-button/cart-button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type CardButtonProps = {}
+
+const CardButton: React.FC<CardButtonProps> = () => {
+    return (
+        <h1>card-button</h1>
+    )
+}
+
+export { CardButton }
